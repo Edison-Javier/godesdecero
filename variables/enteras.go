c4ddf8e703afb1ff3ef100f60d3f0a286ff81d23
Edison Javier Bonilla <edison.javier.bonilla@gmail.com>
2023-05-16 22:44:45 -0500

    feat: First commit

---
+++ variables/enteras.go
@@ -0,0 +1,14 @@
+package variables
+
+import "fmt"
+
+func MostrarEnteros() {
+	var intValue int
+	intVal32 := int32(10)
+	intVal64 := int64(10)
+
+	fmt.Println("intValue = ", intValue)
+	fmt.Println("int32 = ", intVal32)
+	fmt.Println("int64 = ", intVal64)
+
+}
